f0f3a5a539899d6a8a301c81346f841d8c7b6fb2
Rachel Lombardi <37351287+rachellombardi@users.noreply.github.com>
2023-02-20 11:24:42 -0600

    Update find_number_spills.sh

--- find_number_spills.sh
+++ find_number_spills.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Step 1: Redefine arguments from submit file
+# Step 1: Redefine argument from submit file
 dataset=$1
+echo ${dataset}
 
-# Step 2: Count the number of spills by counting the number of lines minus the header row
-# Save output to a variable called `number_spills`
-number_spills= wc -l ${dataset} | awk '{print $1-1}'
-
-# Step 3: Print our dataset and the number of spills to standard out file
-echo ${dataset} ${number_spills}
-
-
+# Step 2: Print out the number of spills by counting the number of lines minus the header row
+wc -l ${dataset} | awk '{print $1-1}'
